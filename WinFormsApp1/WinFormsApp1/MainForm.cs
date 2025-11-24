1fe96c6c010bea2eee97185efe701ff7b8eeb926
tdavis5901 <davist7@tcnj.edu>
2025-11-24 10:15:12 -0500

    skeleton files

---
+++ WinFormsApp1/WinFormsApp1/MainForm.cs
@@ -0,0 +1,15 @@
+namespace WinFormsApp1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
